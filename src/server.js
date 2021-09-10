5570ef472f8f9d43352d0cce4e7d438eaac1faf7
FransKarla <karla.francisco.constantino@gmail.com>
2021-09-10 10:35:38 -0300

    Atualização aula

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 const express = require('express')
 
-const app = express()
+const { MongoClient } = require('mongodb')
 
-const users = []
+let users
+
+const app = express()
 
 app.use(express.json())
 
-app.listen(3000, () => {
+MongoClient.connect('mongodb://localhost:27017/teste').then((cli) => {
+  users = cli.db().collection('users')
+
+  app.listen(3000, () => {
     console.log('server on port 3000')
+  })
+}).catch((error) => {
+  console.error(error)
 })
 
 // app.get('/home', (req, res) => {
@@ -21,9 +29,6 @@ app.listen(3000, () => {
 
 //     res.send('Vitória\' Home')
 // })
-
-
-
 
 // if (typeof req.body.username !== 'string' || typeof req.body.password !== 'string' || typeof req.body.email !== 'string'){
 //     res.status(400).send('Parâmetros inválidos')
@@ -40,114 +45,138 @@ app.listen(3000, () => {
 //     return
 // }
 
-
-
-
 app.post('/login', (req, res) => {
-    console.log(req.body)
+  console.log(req.body)
 
-    if (typeof req.body.username !== 'string' || typeof req.body.password !== 'string') {
-        res.status(400).send('Parâmetros inválidos')
-        return
+  if (typeof req.body.username !== 'string' || typeof req.body.password !== 'string') {
+    res.status(400).send('Parâmetros inválidos')
+    return
+  }
+
+  users.findOne({ username: req.body.username }).then((result) => {
+    if (result === null) {
+      res.status(400).send('Usuário inexistente!')
+
+      return
     }
 
-    const result = users.find(element => {
-        return element.username === req.body.username && element.password === req.body.password
-    })
+    if (result.password !== req.body.password) {
+      res.status(400).send('Senha inválida!')
 
-    if (typeof result === 'undefined') {
-        res.status(400).send('usuário ou senha incorretos!')
-        return
+      return
     }
-    res.send('Ok')
+
+    res.send('Ok!')
+  }).catch(() => {
+    res.send('Error')
+  })
 })
-
-
 
 app.post('/user', (req, res) => {
-    console.log(req.body)
+  console.log(req.body)
 
-    const requiresFields = ['username', 'email', 'password', 'passwordConfirmation']
+  const requiresFields = ['username', 'email', 'password', 'passwordConfirmation']
 
-    for (const key of requiresFields) {
-        if (typeof req.body[key] !== 'string') {
-            res.status(400).send(`Parâmetro inválido: ${key}`)
-            return
-        }
+  for (const key of requiresFields) {
+    if (typeof req.body[key] !== 'string') {
+      res.status(400).send(`Parâmetro inválido: ${key}`)
+      return
+    }
+  }
 
+  if (req.body.password !== req.body.passwordConfirmation) {
+    res.status(400).send('Confirmação de senha incorreta!')
+    return
+  }
+
+  users.findOne({ username: req.body.username }).then((result) => {
+    console.log(result)
+    if (result !== null) {
+      res.status(400).send('Usuário já existente!')
+
+      return
     }
 
-
-
-    if (req.body.password !== req.body.passwordConfirmation) {
-        res.status(400).send(`Confirmação de senha incorreta!`)
-        return
-    }
-
-    const result = users.find(user => {
-        return user.username === req.body.username && user.email === req.body.email
+    users.insertOne({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    }).then((result) => {
+      res.send('Ok')
+    }).catch(() => {
+      res.send('Error')
     })
+  }).catch(() => {
+    res.send('Error')
+  })
 
-    if (typeof result !== 'undefined') {
-        res.status(400).send('Usuário já existente!')
-        return
-    }
-
-    users.push({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    })
-
-    res.send('Ok')
+  // users.push({
+  //   username: req.body.username,
+  //   email: req.body.email,
+  //   password: req.body.password
+  // })
 })
 
-app.put('/user/:id', (req, res) => { //req - requisição (é o q o usuario mandou ao servidor) e o res - resposta (resposta do servidor ao usuario)   => (arrow function - função)
-    // console.log(req.params.id)
+app.put('/user/:id', (req, res) => { // req - requisição (é o q o usuario mandou ao servidor) e o res - resposta (resposta do servidor ao usuario)   => (arrow function - função)
+  // console.log(req.params.id)
 
-    if (req.body.password === "") {
-        res.status(400).send(`Senha inválida`)
-        return
+  if (req.body.password === '') {
+    res.status(400).send('Senha inválida')
+    return
+  }
+
+  // users.forEach((user) => { //user - é o usuário que será rodado
+  //     if (user.username === req.params.id) { //
+  //         user.password = req.body.password //depois de achar o usuário, será possível mudar a senha, user.password (senha atual) será substituída pela nova senha atribuida ao req.body.password
+  //         res.send(`Atualizar senha ${req.params.id}`)
+  //     }
+  // })
+  // res.status(400).send(`Usuário não encontrado`)
+
+  // const index = users.findIndex((user) => {
+  //   return user.username === req.params.id
+  // })
+
+  users.updateOne({ username: req.params.id }, { $set: { password: req.body.password } }).then((result) => {
+    if (result.matchedCount === 0) {
+      res.status(400).send('Usuário não encontrado!')
+      return
     }
-
-    // users.forEach((user) => { //user - é o usuário que será rodado
-    //     if (user.username === req.params.id) { //
-    //         user.password = req.body.password //depois de achar o usuário, será possível mudar a senha, user.password (senha atual) será substituída pela nova senha atribuida ao req.body.password
-    //         res.send(`Atualizar senha ${req.params.id}`)
-    //     }
-    // })
-    // res.status(400).send(`Usuário não encontrado`)
-
-    const index = users.findIndex((user) => {
-        return user.username === req.params.id
-    })
-
-    if (index === -1) {
-        res.status(400).send(`Usuário não encontrado`)
-    }
-
-    users[index].password = req.body.password
     res.send(`Atualizar senha de ${req.params.id}`)
+  }).catch(() => {
+    res.send('Error!')
+  })
 })
 
 app.delete('/user/:id', (req, res) => {
+  // const index = users.findIndex((user) => { // index - indice do array (posição do elemtno no array)
+  //   return user.username === req.params.id
+  // })
 
-    const index = users.findIndex((user) => {   //index - indice do array (posição do elemtno no array)
-        return user.username === req.params.id
-    })
+  // if (index === -1) {
+  //   res.status(400).send('Usuário não encontrado!')
+  // }
 
-    if (index === -1) {
-        res.status(400).send('Usuário não encontrado!')
+  // users[index].username = req.params.id
+  // users.splice(index, 1) // splice - vai cortar um pedaço do array
+
+  users.deleteOne({ username: req.params.id }).then((result) => {
+    if (result.deletedCount === 0) {
+      res.status(400).send('Usuário não encontrado!')
+      return
     }
-
-    users[index].username = req.params.id
-    users.splice(index, 1) //splice - vai cortar um pedaço do array
     res.send(`O usuário ${req.params.id} foi deletado`)
-
-});
-
-app.get('/user', (req, res) => {
-
-    res.json(users)
+  }).catch(() => {
+    res.send('Error!')
+  })
 })
 
+app.get('/user', (req, res) => {
+  // res.json(users)
+  const cursor = users.find()
+  cursor.toArray().then((result) => {
+    res.json(result)
+  }).catch(() => {
+    res.send('Error!')
+  })
+})
